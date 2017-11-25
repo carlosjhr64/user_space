672d48fa6b8c1e7f811de8325b83146f6b74eea0
carlosjhr64 <carlosjhr64@gmail.com>
2017-11-25 15:55:21 -0800

    clean up to version 3, ruby 2.4.2.

--- lib/user_space.rb
+++ lib/user_space.rb
@@ -1,7 +1,134 @@
 require 'fileutils'
-require 'json' # default parser
 require 'xdg'
-require 'user_space/version.rb'
-require 'user_space/user_space.rb'
 # Requires:
 #`ruby`
+
+module FileUtils
+  class << self
+    def user_space_cpr(src, dest)
+      fu_each_src_dest(src, dest) do |s, d|
+        copy_entry(s, d)
+        chmod('u+rwX,go-rwx', d)
+      end
+    end
+  end
+end
+
+class UserSpace
+  VERSION = '3.0.0'
+
+  def self.appdir
+    appdir = File.dirname File.dirname caller_locations(1,1)[0].path
+    appdir = File.dirname appdir if File.basename(appdir)=='lib'
+    File.expand_path appdir
+  end
+
+  attr_reader :appname, :appdir, :xdgbases, :options
+  def initialize(
+    parser,
+    ext: parser.to_s.downcase,
+    appname: File.basename($0),
+    xdgbases: ['CACHE', 'CONFIG', 'DATA'],
+    appdir: UserSpace.appdir,
+    config: 'config',
+    version: 'VERSION'
+  )
+    @parser,@ext,@appname,@xdgbases,@appdir,@config,@version = parser,ext,appname,xdgbases,appdir,config,version
+    install(false) # install with no overwrite
+  end
+
+  def xdg_pairs
+    @xdgbases.each do |base|
+      xdg = XDG[base].to_s
+      userdir = File.join(xdg, @appname)
+      basedir = File.join @appdir, base.downcase
+      yield basedir, userdir
+    end
+  end
+
+  # Note that initialize will not overwrite anything.
+  # This overwrites the user's data directory with a fresh install.
+  # App should consider being nice about this,
+  # like warn the user or something.
+  def install(overwrite=true)
+    xdg_pairs do |basedir, userdir|
+      if File.exist?(userdir)
+        # Sanity check
+        raise "Not a directory: #{userdir}" unless File.directory?(userdir)
+        # Pre-existing directory.
+        # Return unless user wants to overwrite.
+        next unless overwrite
+      else
+        Dir.mkdir(userdir, 0700)
+      end
+      FileUtils.user_space_cpr(Dir.glob("#{basedir}/*"), userdir) if File.directory? basedir
+    end
+  end
+
+  def cachedir
+    File.join XDG['CACHE'].to_s, @appname
+  end
+
+  def configdir
+    File.join XDG['CONFIG'].to_s, @appname
+  end
+
+  def datadir
+    File.join XDG['DATA'].to_s, @appname
+  end
+
+  # Not really for public use.
+  def config_file_name
+    File.join XDG['CONFIG'].to_s, @appname, "#{@config}.#{@ext}"
+  end
+
+  # Not really for public use.
+  def version_file_name
+    File.join XDG['DATA'].to_s, @appname, @version
+  end
+
+  def config?
+    File.exist?(config_file_name)
+  end
+
+  # Reads config
+  def config
+    @parser.load File.read(config_file_name)
+  rescue
+    $stderr.puts $!.message   if $VERBOSE
+    $stderr.puts $!.backtrace if $DEBUG
+    nil
+  end
+
+  # Saves config
+  def config=(obj)
+    dump = (@parser.respond_to?(:pretty_generate))? @parser.pretty_generate(obj) : @parser.dump(obj)
+    File.open(config_file_name, 'w', 0600){|fh|fh.puts dump}
+  end
+
+  def configures(hash)
+    if self.config? # file exists
+      self.config.each{|opt, value| hash[opt.to_sym] = value}
+    else
+      $stderr.puts config_file_name if $VERBOSE
+      self.config = hash
+    end
+  end
+
+  def version?
+    File.exist?(version_file_name)
+  end
+
+  # This reads the data directory's version file
+  def version
+    File.read(version_file_name).strip
+  rescue
+    $stderr.puts $!.message   if $VERBOSE
+    $stderr.puts $!.backtrace if $DEBUG
+    nil
+  end
+
+  def version=(v)
+    File.open(version_file_name, 'w', 0600){|fh| fh.puts v}
+  end
+end
